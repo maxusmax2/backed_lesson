2b0e1d6b7ca9f4d8f408c4bc7a4f2582e5bf02f0
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2022-08-01 19:04:35 +0300

    Базовая функциональность отдачи деталки
    добавлена функция getDetail контроллера DetailController,пока без функции
     отправки изображений и кеширования

--- course-project/app/Http/Controllers/DetailController.php
+++ course-project/app/Http/Controllers/DetailController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Apartament;
+use App\Models\Commercial;
+use App\Models\Cottage;
+use App\Models\Land;
 use Illuminate\Http\Request;
+use App\Models\Room;
 
 class DetailController extends Controller
 {
-    //
+    /**
+     * return JSON with detail build,and image
+     * attribute buildType and id build
+     */
+    function getDetail(string $buildType, int  $id){
+
+        switch ($buildType){
+            case 'rooms':
+                return Room::find($id)->get();
+            case 'apartaments':
+                return Apartament::find($id)->get();
+            case 'land':
+                return Land::find($id)->get();
+            case 'Cottage':
+                return Cottage::find($id)->get();
+            case 'Commercial':
+                return  Commercial::find($id)->get();
+        }
+    }
 }
